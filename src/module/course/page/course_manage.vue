83d4e662c505c0fa978bfa97638387604dceca6d
Xiaodong Sun <xiaodong.sun@61topbaby.com>
2019-03-14 14:31:27 +0800

    在线教学管理前端工程

---
+++ src/module/course/page/course_manage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#eee"
+      text-color="#000"
+      active-text-color="#000"
+    >
+      <el-menu-item index="1">
+        <router-link class="mui-tab-item" :to="{path:'/course/manage/summary/'+this.courseid}">课程首页</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link class="mui-tab-item" :to="{path:'/course/manage/baseinfo/'+this.courseid}">基本信息</router-link>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <router-link class="mui-tab-item" :to="{path:'/course/manage/picture/'+this.courseid}">课程图片</router-link>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <router-link
+          class="mui-tab-item"
+          :to="{path:'/course/manage/marketinfo/'+this.courseid}"
+        >课程营销</router-link>
+      </el-menu-item>
+      <el-menu-item index="5">
+        <router-link class="mui-tab-item" :to="{path:'/course/manage/plan/'+this.courseid}">课程计划</router-link>
+      </el-menu-item>
+      <el-menu-item index="6">
+        <router-link class="mui-tab-item" :to="{path:'/course/manage/teacher/'+this.courseid}">教师信息</router-link>
+      </el-menu-item>
+      <el-menu-item index="7">
+        <router-link class="mui-tab-item" :to="{path:'/course/manage/pub/'+this.courseid}">发布课程</router-link>
+      </el-menu-item>
+    </el-menu>
+    <router-view class="main"></router-view>
+  </div>
+</template>
+<script>
+import * as courseApi from "../api/course";
+import utilApi from "../../../common/utils";
+export default {
+  data() {
+    return {
+      activeIndex: "2",
+      courseid: ""
+    };
+  },
+  methods: {},
+  mounted() {
+    //课程id
+    this.courseid = this.$route.params.courseid;
+
+    console.log("courseid=" + this.courseid);
+    //跳转到课程基本信息
+    this.$router.push({ path: "/course/manage/baseinfo/" + this.courseid });
+  }
+};
+</script>
+<style scoped>
+</style>
